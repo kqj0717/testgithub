9a5735c5acc9c5da7b7f844e52c1f31a5ac6b054
kongqingjing <wb-kqj263975@alibaba-inc.com>
2017-02-17 15:11:00 +0800

    update01

---
+++ src/com/testguthub/TestPackage.java
@@ -0,0 +1,12 @@
+package com.testguthub;
+
+import org.eclipse.jdt.internal.compiler.batch.Main;
+
+public class TestPackage {
+     
+    public static void main(String[] args) {
+		
+    	System.out.println("这是测试egit 到GitHub的上传功能");
+    	
+	}
+}
